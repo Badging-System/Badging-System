c3e15d2f0abc5ebb7a6096f4ab59952defa30b31
gndeshpa <gauravd257@gmail.com>
2019-10-23 16:20:26 -0700

    Testinig an insert

--- server/src/database/mongoDB.js
+++ server/src/database/mongoDB.js
@@ -13,11 +13,22 @@ class Mongo
     {
       assert.equal( null, err );
       console.log( "Successfully connected to MongoDB..." );
+      // document to be inserted
+      var doc = {name: "Roshan", age: "22"};
+      db.collection( "User" ).insertOne( doc, function ( err, res )
+      {
+        if ( err ) throw err;
+        console.log( "Document inserted" );
+        // close the connection to db when you are done with it
+        db.close();
+      } );
 
-      db.close();
     } );
 
   }
+
+
+
 }
 
 module.exports = {Mongo};
