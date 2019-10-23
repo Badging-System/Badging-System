7a62ecf9d75f4be1f0e18c0ccc5ad4365e657ff7
gndeshpa <gauravd257@gmail.com>
2019-10-22 21:22:24 -0700

    Created MongoDB base connection

---
+++ server/src/database/mongoDB.js
@@ -0,0 +1,23 @@
+class Mongo
+{
+  connectMongoDB ()
+  {
+    var MongoClient = require( 'mongodb' ).MongoClient
+
+    var assert = require( 'assert' )
+    // URl that connections to mongoDB
+    var url = 'mongodb://localhost:27017/badging-system'
+
+    // Connection to mongoDB
+    MongoClient.connect( url, function ( err, db )
+    {
+      assert.equal( null, err );
+      console.log( "Connected successfully to server" );
+
+      db.close();
+    } );
+
+  }
+}
+
+module.exports = {Mongo};
