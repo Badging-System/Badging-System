bb24984e9af2ea640fdf7b7fb29ae9251c4e0772
gndeshpa <gauravd257@gmail.com>
2019-10-23 23:38:44 -0700

    Moved mongoDB connection call into www file. Also added useUnifiedTopology to true in connect parameter to remove deprecated message

--- server/src/database/mongoDB.js
+++ server/src/database/mongoDB.js
@@ -2,37 +2,21 @@ class Mongo
 {
   connectMongoDB ()
   {
-    const MongoClient = require( 'mongodb' ).MongoClient
-    const test = require( 'assert' );
+    const MongoClient = require('mongodb').MongoClient
+    const test = require('assert');
 
     // URL to connect to mongoDB locally
-    const url = 'mongodb://localhost:27018/badging-system';
+    const url = 'mongodb://localhost:27018/';
     const dbName = 'badging-system'
     // Connection to mongoDB
-    MongoClient.connect( url, function ( err, client )
+    MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client)
     {
-      const col = client.db( dbName ).collection( 'User' );
-      // const collection = db.collection( 'User' );
+      // const collection = client.db( dbName ).collection( 'User' );
       // console.log( "Switched to " + db.databaseName + " database" );
-      if ( err ) throw err;
-      console.log( "Successfully connected to MongoDB..." );
-      // document to be inserted
-      col.insert( [{a: 1, b: 1}
-        , {a: 2, b: 2}, {a: 3, b: 3}
-        , {a: 4, b: 4}], {w: 1}, function ( err, result )
-      {
-        test.equal( null, err );
-        // Show that duplicate records got dropped
-        col.aggregation( {}, {cursor: {}} ).toArray( function ( err, items )
-        {
-          test.equal( null, err );
-          test.equal( 4, items.length );
-          client.close();
-        } );
-      } );
+      if (err) throw err;
+      console.log("Successfully connected to MongoDB...");
 
-
-    } );
+    });
 
   }
 
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,15 +1,15 @@
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
 
-const UserRoutes = require( "./user/UserRoutes" );
+const UserRoutes = require("./user/UserRoutes");
 
 
 /* Handles all routes associated the endpoint api */
-router.use( '/users', UserRoutes );
+router.use('/users', UserRoutes);
 
-router.get( '/', function ( req, res, next )
+router.get('/', function (req, res, next)
 {
-  res.send( 'Welcome to the Badging System API' );
-} );
+  res.send('Welcome to the Badging System API');
+});
 
 module.exports = router;
