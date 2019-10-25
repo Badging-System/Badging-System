0cde7ce90944448e3ffcf7c5d0d7aed566c2c05b
gndeshpa <gauravd257@gmail.com>
2019-10-25 16:59:16 -0700

    Structured post request and method to handle adding a new user.

--- server/src/routes/user/UserController.js
+++ server/src/routes/user/UserController.js
@@ -1,19 +1,32 @@
 const JSONResponse = require('../../service/response/JSONResponse');
 const InvalidInput = require('../../service/response/InvalidInput');
+const User = require('../../../models/User')
 
-exports.index = (req, res) => {
+exports.index = (req, res) =>
+{
   JSONResponse(res, {
     message: 'Success'
-  });
+  }, 200);
 };
 
-exports.user_id = (req, res) => {
+exports.user_id = (req, res) =>
+{
   let userId = req.params.id;
-  if (!userId) {
+  if (!userId)
+  {
     InvalidInput(res, 'Invalid Input');
-  } else {
+  } else
+  {
     JSONResponse(res, {
       message: 'Success ' + userId
-    });
+    }, 200);
   }
 };
+
+exports.addUser = (req, res) =>
+{
+  console.log(req.body)
+  let addedUser = new User(req.body)
+  console.log(`this is the first name of the added user: ${addedUser.First_name}`)
+  JSONResponse(res, {message: 'User succesfully added!'}, 201)
+}; 
