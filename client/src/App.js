07270734e1d7dc71b175de861d6ccd4d66b66345
dmaitha <dmaitha@asu.edu>
2019-10-31 20:53:00 -0700

    initial additional of react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomeText} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function HomeText() {
+  return <h1>Home Page </h1>;
 }
 
 export default App;
