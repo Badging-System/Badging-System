b424858cc5b504620732649d1e372022803bfb69
Mitchell <Mitchell.S.Roberts@asu.edu>
2019-11-06 14:29:33 -0700

    Added promises to test cases for Travis CI error

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const JSONResponse = require('../service/response/JSONResponse');
+const InvalidInput = require('../service/response/InvalidInput');
 
 const UserRoutes = require("./user/UserRoutes");
 
@@ -8,7 +10,9 @@ const UserRoutes = require("./user/UserRoutes");
 router.use('/users', UserRoutes);
 
 router.get('/', function(req, res, next) {
-  res.send('Welcome to the Badging System API');
+  JSONResponse(res, {
+    message: 'Welcome to the Badging System API'
+  }, 200);
 });
 
 module.exports = router;
--- server/test/test-endpoints.js
+++ server/test/test-endpoints.js
@@ -1,32 +1,31 @@
 const chai = require('chai');
 var expect = require('chai').expect;
-var request = require('request');
-const chaiHttp = require('chai-http');
+var request = require('request-promise');
 const async = require('async');
-chai.use(chaiHttp);
 
 require('dotenv').config()
 
 /* This test the main page response to ensure the response is correct */
 it('Main page content', function(done) {
-  request('http://localhost:8080/api/', function(error, response, body) {
-    expect(response.statusCode).to.equal(200);
-    expect(body).to.equal('Welcome to the Badging System API');
+  this.timeout(15000);
+  request('http://localhost:8080/api/').then((response) => {
+    let parsedRes = JSON.parse(response)
+    expect(parsedRes.status).to.equal(200);
+    expect(parsedRes.payload.message).to.equal('Welcome to the Badging System API');
     done();
-  });
+  }).catch(done);
 });
 
 /* This test that the endpoint returns the correct type of object */
 describe('User Endpoints', function() {
-
   /* This test the user endpoint testing if the it recieve the id poarameter */
   it('User Query Param', function(done) {
     this.timeout(15000);
-    request('http://localhost:8080/api/users/msrober', function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      let res = JSON.parse(response.body); //parse payload
+    request('http://localhost:8080/api/users/msrober').then((response) => {
+      let parsedRes = JSON.parse(response)
+      expect(parsedRes.status).to.equal(200);
       done();
-    });
+    }).catch(done);
   });
 
   //Only run this test if the enviroment is DEV
@@ -34,13 +33,12 @@ describe('User Endpoints', function() {
     /* This test that the database is seeded with the correct amount of users in the development enviroment */
     it('should list the seeded database', function(done) {
       this.timeout(15000);
-      request('http://localhost:8080/api/users/', function(error, response, body) {
-        console.log(response);
-        expect(response.statusCode).to.equal(200);
-        let res = JSON.parse(response.body); //parse payload
-        expect(res.payload.data).to.have.lengthOf(5);
+      request('http://localhost:8080/api/users/').then((response) => {
+        let parsedRes = JSON.parse(response)
+        expect(parsedRes.status).to.equal(200);
+        expect(parsedRes.payload.data).to.have.lengthOf(5);
         done();
-      });
+      }).catch(done);
     });
   });
 
@@ -49,22 +47,21 @@ describe('User Endpoints', function() {
     /* This test that the endpoint returns the correct type of object */
     it('User Endpoint', function(done) {
       this.timeout(15000);
-      request('http://localhost:8080/api/users/msrober', function(error, response, body) {
-        console.log(response);
-        expect(response.statusCode).to.equal(200);
-        let res = JSON.parse(response.body); //parse payload
-        expect(res.payload.data[0])
+      request('http://localhost:8080/api/users/msrober').then((response) => {
+        let parsedRes = JSON.parse(response) //parse payload
+        expect(parsedRes.status).to.equal(200);
+        expect(parsedRes.payload.data[0])
           .to.have.property('Username', 'msrober')
-        expect(res.payload.data[0])
+        expect(parsedRes.payload.data[0])
           .to.have.property('First_name', 'Mitchell')
-        expect(res.payload.data[0])
+        expect(parsedRes.payload.data[0])
           .to.have.property('Last_name', 'Roberts')
-        expect(res.payload.data[0])
+        expect(parsedRes.payload.data[0])
           .to.have.property('Status', 'User')
-        expect(res.payload.data[0])
+        expect(parsedRes.payload.data[0])
           .to.have.property('Email', 'msrober@gmail.com')
         done();
-      });
+      }).catch(done);
     });
   });
 });
