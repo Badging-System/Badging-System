81e0311dffa0d99e77ce6649cb26d1394b4fdc74
gndeshpa <gauravd257@gmail.com>
2019-11-08 18:32:14 -0700

    Added logic to validate username and email are both unique before entering a new user or users into the database. Wrote javascript comments as well

--- server/src/routes/user/UserController.js
+++ server/src/routes/user/UserController.js
@@ -6,31 +6,39 @@ const {
 } = require("../../database/mongoDB");
 
 const mongoDB = new Mongo();
-exports.index = (req, res) => {
+exports.index = (req, res) =>
+{
   //Get all the collection data based off the User model
-  mongoDB.getCollectionData(User).then(async (data) => {
+  mongoDB.getCollectionData(User).then(async (data) =>
+  {
     JSONResponse(res, {
       data: data
     }, 200);
-  }).catch((error) => {
+  }).catch((error) =>
+  {
     JSONResponse(res, {
       error: error
     }, 500);
   });
 };
 
-exports.user_id = (req, res) => {
+exports.user_id = (req, res) =>
+{
   let userId = req.params.id;
-  if (!userId) {
+  if (!userId)
+  {
     InvalidInput(res, 'No username was provided.');
-  } else {
+  } else
+  {
     mongoDB.findOne(User, {
       Username: userId
-    }).then(async (data) => {
+    }).then(async (data) =>
+    {
       JSONResponse(res, {
         data: data
       }, 200);
-    }).catch((error) => {
+    }).catch((error) =>
+    {
       JSONResponse(res, {
         error: error
       }, 500);
@@ -38,71 +46,75 @@ exports.user_id = (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-exports.addUser = (req, res) =>
+exports.addUser = async (req, res) =>
 {
-  let collection = 'users';
 
   let addedUser = new User(req.body);
+
 
   if ((addedUser.hasOwnProperty('Username')) && (addedUser.hasOwnProperty('First_name')) && (addedUser.hasOwnProperty('Last_name')) && (addedUser.hasOwnProperty('Status')) && (addedUser.hasOwnProperty('Email')))
   {
-    JSONResponse(res, {message: "Invalid Request"}, 400);
-  }
-  else if (addedUser.Status !== 'User')
-  {
-    JSONResponse(res, {message: 'Status needs to be set to User. Document was not inserted.'}, 403);
-  }
-  else
-  {
-    mongoDB.insertOneDocument(collection, addedUser);
-    JSONResponse(res, {message: addedUser}, 201);
-
-=======
-exports.addUser = (req, res) => {
-
-  let addedUser = new User(req.body);
-
-  if ((addedUser.hasOwnProperty('Username')) && (addedUser.hasOwnProperty('First_name')) && (addedUser.hasOwnProperty('Last_name')) && (addedUser.hasOwnProperty('Status')) && (addedUser.hasOwnProperty('Email'))) {
     JSONResponse(res, {
       message: "Invalid Request"
     }, 400);
-  } else if (addedUser.Status !== 'User') {
+  } else if (addedUser.Status !== 'User')
+  {
     JSONResponse(res, {
       message: 'Status needs to be set to User. Document was not inserted.'
     }, 403);
-  } else {
-    JSONResponse(res, {
-      message: addedUser
-    }, 201);
->>>>>>> dev
+  } else
+  {
+    var result = await mongoDB.validateUserNameEmail(User, addedUser);
+    if (result === null)
+    {
+      JSONResponse(res, {
+        message: addedUser
+      }, 201);
+    } else
+    {
+      JSONResponse(res, {
+        message: "Username and/or email already exists."
+      }, 403);
+    }
+
+
   }
 };
 
-exports.addedUsers = (req, res) => {
+exports.addedUsers = async (req, res) =>
+{
   let addedUsers = req.body;
 
-  if ((addedUsers.hasOwnProperty('Username')) && (addedUsers.hasOwnProperty('First_name')) && (addedUsers.hasOwnProperty('Last_name')) && (addedUsers.hasOwnProperty('Status')) && (addedUsers.hasOwnProperty('Email'))) {
+  if ((addedUsers.hasOwnProperty('Username')) && (addedUsers.hasOwnProperty('First_name')) && (addedUsers.hasOwnProperty('Last_name')) && (addedUsers.hasOwnProperty('Status')) && (addedUsers.hasOwnProperty('Email')))
+  {
     JSONResponse(res, {
       message: "Invalid Request"
     }, 400);
 
-  } else {
-    for (index in addedUsers) {
-      if (addedUsers[index].Status !== 'User') {
+  } else
+  {
+    for (index in addedUsers)
+    {
+      if (addedUsers[index].Status !== 'User')
+      {
         JSONResponse(res, {
           message: 'Status needs to be set to User. No documents were inserted.'
         }, 403);
         return;
       }
     }
-<<<<<<< HEAD
+    var result = await mongoDB.validateUserNameEmail(User, addedUsers);
+    if (result === null)
+    {
+      JSONResponse(res, {
+        message: addedUsers
+      }, 201);
+    } else
+    {
+      JSONResponse(res, {
+        message: "Username and/or email already exists for one or more entered users."
+      }, 403);
+    }
 
-    JSONResponse(res, {message: addedUsers}, 201);
-=======
-    JSONResponse(res, {
-      message: addedUsers
-    }, 201);
->>>>>>> dev
   }
 }
