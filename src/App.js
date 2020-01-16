ea48fa8faa61d8ca2fd3387d8de4dca633a9ab1e
gndeshpa <gauravd257@gmail.com>
2020-01-15 19:55:25 -0700

    Added state to Login Page to handle radio button selection. Added a default User page and added route to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./home/Home";
 import Login from "./Login/Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {withStyles} from "@material-ui/core/styles";
 import Admin from "./views/Admin";
+import User from "./views/User";
 
 class App extends React.Component {
   render() {
@@ -13,9 +14,10 @@ class App extends React.Component {
           <Route exact path='/' component={Login} />
           <Route exact path='/home' component={Home} />
           <Route exact path='/admin' component={Admin} />
+          <Route exact path='/user' component={User} />
         </Switch>
       </Router>
     );
   }
 }
-export default withStyles({ withTheme: true })(App);
+export default withStyles({withTheme: true})(App);
