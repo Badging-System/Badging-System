24429078bbc5c18308455fcd9010c9dac3e1a865
dmaitha <dmaitha@asu.edu>
2020-01-16 21:14:54 -0700

    created coach view component and added to react router in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from "./home/Home";
 import Login from "./Login/Login";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {withStyles} from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 import Admin from "./views/Admin";
 import User from "./views/User";
+import Coach from "./views/Coach";
 
 class App extends React.Component {
   render() {
@@ -15,9 +16,10 @@ class App extends React.Component {
           <Route exact path='/home' component={Home} />
           <Route exact path='/admin' component={Admin} />
           <Route exact path='/user' component={User} />
+          <Route exact path='/coach' component={Coach} />
         </Switch>
       </Router>
     );
   }
 }
-export default withStyles({withTheme: true})(App);
+export default withStyles({ withTheme: true })(App);
