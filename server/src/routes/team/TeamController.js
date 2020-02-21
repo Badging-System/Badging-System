7504eef7e95d3895125dd9859f44c3554fe7f7df
Mitchell <Mitchell.S.Roberts@asu.edu>
2020-02-21 11:25:32 -0700

    Completed team table integration

--- server/src/routes/team/TeamController.js
+++ server/src/routes/team/TeamController.js
@@ -3,21 +3,34 @@ const Team = require("../../../models/Team");
 const User = require("../../../models/User");
 const {Mongo} = require("../../database/mongoDB");
 const mongoose = require("mongoose");
+const moment = require("moment");
+
 
 const mongoDB = new Mongo();
 
 exports.index = (req, res) => {
   //Get all the collection data based off the Team model
-  console.log("HERE");
   mongoDB.getTeamInfo()
     .then(data => {
-      console.log("done");
-      JSONResponse(
-        res, {
-          data: data
-        },
-        200
-      );
+      if (req.query.table_data) {
+        table_format(data).then((formatted_data) => { //format the data to table data if requested
+          JSONResponse(
+            res, {
+              data: formatted_data
+            },
+            200
+          );
+        }).catch((err) => {
+          console.log(err);
+        }); 
+      } else {
+        JSONResponse(
+          res, {
+            data: data
+          },
+          200
+        );
+      }
     })
     .catch(err => {
       console.log(err);
@@ -97,6 +110,15 @@ exports.addTeamMember = async (req, res) => {
 };
 
 //format data to match table
-function table_format(response) {
-  return new Promise((resolve, reject) => {});
+function table_format(data) {
+  return new Promise((resolve, reject) => {
+    const table_data =  data.map(team => ({
+      name: team.Name,
+      coach: team.Coach,
+      playerCount: team.Members.length,
+      createdOn: moment().format(),
+      approved: team.Approved
+    }));
+    resolve(table_data);
+  });
 }
