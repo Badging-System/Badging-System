e6c7f0032ff87aa852c9998b33195806eeaea42c
dmaitha <dmaitha@asu.edu>
2020-02-22 16:12:38 -0700

    fixes

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MaterialTable from "material-table";
 import { forwardRef } from "react";
 import AddBox from "@material-ui/icons/AddBox";
