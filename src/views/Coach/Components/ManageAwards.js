a76f55f2f3148a88b3aeb04789cfd55c3db5413c
Mitchell <Mitchell.S.Roberts@asu.edu>
2020-02-26 14:48:53 -0700

    Added modal to award tab with some skeleton data

--- src/views/Coach/Components/ManageAwards.js
+++ src/views/Coach/Components/ManageAwards.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import {Button} from "@material-ui/core"
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ProgressStepper from "./ProgressStepper";
 import { Typography } from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
+import SpringModal from "./Modal";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,8 +18,22 @@ const useStyles = makeStyles(theme => ({
 
 export default function FolderList() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+      Create Badge
+    </Button>
+    <SpringModal open={open} handleClose={handleClose}/>
     <List className={classes.root}>
       <ListItem>
         <Typography>Member 1</Typography>
@@ -29,5 +45,6 @@ export default function FolderList() {
         <ProgressStepper />
       </ListItem>
     </List>
+    </div>
   );
 }
