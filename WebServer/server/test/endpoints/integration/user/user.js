5643b3bb5b85b78fc0a369a45de6f54b00c36ad0
Mitchell <Mitchell.S.Roberts@asu.edu>
2020-03-20 13:08:25 -0700

    Update user.js

--- WebServer/server/test/endpoints/integration/user/user.js
+++ WebServer/server/test/endpoints/integration/user/user.js
@@ -7,28 +7,28 @@ var app = require("../../../../app");
 var server;
 var http = require("http");
 var debug = require("debug")("server:server");
-var seed = require("../../../../Seed/seed.js").seedDB;
+// var seed = require("../../../../Seed/seed.js").seedDB;
 
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "./.env") });
 
 describe("Integration Testing", function() {
   this.timeout(15000);
-  before(done => {
-    var port = parseInt(process.env.PORT || "3000", 10);
-    app.set("port", port);
-    server = http.createServer(app);
-    server.listen(port, "localhost", function() {
-      seed(done);
-    });
-  });
+  // before(done => {
+  //   var port = parseInt(process.env.PORT || "3000", 10);
+  //   app.set("port", port);
+  //   server = http.createServer(app);
+  //   server.listen(port, "${process.env.HOST}", function() {
+  //     seed(done);
+  //   });
+  // });
 
   /* This test that the endpoint returns the correct type of object */
   describe("User Endpoints", function() {
     /* This test the user endpoint testing if the it recieve the id poarameter */
     it("User Query Param", function(done) {
       this.timeout(15000);
-      request(`http://localhost:${process.env.PORT}/api/users/msrober`)
+      request(`http://${process.env.HOST}${process.env.PORT}/api/users/msrober`)
         .then(response => {
           let parsedRes = JSON.parse(response);
           expect(parsedRes.status).to.equal(200);
@@ -43,7 +43,7 @@ describe("Integration Testing", function() {
       // Post a user object to the database
 
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/adduser`, {
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/adduser`, {
           Username: "dbooker",
           First_name: "devin",
           Last_name: "booker",
@@ -67,7 +67,7 @@ describe("Integration Testing", function() {
       // Post a user object to the database
 
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/adduser`, {
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/adduser`, {
           Username: "dbooker",
           First_name: "devin",
           Last_name: "booker",
@@ -91,7 +91,7 @@ describe("Integration Testing", function() {
       // Post a user object to the database
 
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/adduser`, {
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/adduser`, {
           Username: "dbooker",
           First_name: "devin",
           Last_name: "booker",
@@ -114,7 +114,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbooker",
             First_name: "Devin",
@@ -155,7 +155,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbooker",
             First_name: "Devin",
@@ -196,7 +196,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbooker",
             First_name: "Devin",
@@ -238,7 +238,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbooker",
             First_name: "Devin",
@@ -279,7 +279,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbooker",
             First_name: "Devin",
@@ -320,7 +320,7 @@ describe("Integration Testing", function() {
       this.timeout(15000);
       // Post a user object to the database
       axios
-        .post(`http://localhost:${process.env.PORT}/api/users/addedusers`, [
+        .post(`http://${process.env.HOST}${process.env.PORT}/api/users/addedusers`, [
           {
             Username: "dbook",
             First_name: "Devin",
@@ -363,7 +363,7 @@ describe("Integration Testing", function() {
         /* This test that the database is seeded with the correct amount of users in the development enviroment */
         it("should list the seeded database", function(done) {
           this.timeout(15000);
-          request(`http://localhost:${process.env.PORT}/api/users/`)
+          request(`http://${process.env.HOST}${process.env.PORT}/api/users/`)
             .then(response => {
               let parsedRes = JSON.parse(response);
               expect(parsedRes.status).to.equal(200);
@@ -382,7 +382,7 @@ describe("Integration Testing", function() {
         /* This test that the endpoint returns the correct type of object */
         it("should return the correct type of object (User)", function(done) {
           this.timeout(15000);
-          request(`http://localhost:${process.env.PORT}/api/users/msrober`)
+          request(`http://${process.env.HOST}${process.env.PORT}/api/users/msrober`)
             .then(response => {
               let parsedRes = JSON.parse(response); //parse payload
               expect(parsedRes.status).to.equal(200);
